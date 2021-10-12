12724f33eda8ae63021bf83bbdca4d230a362d01
fika4life <fistewie@gmail.com>
2021-10-12 10:27:06 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import ContactCard from "./ContactCard.svelte"
+
+	let name = ''
+	let jobTitle, description, imgUrl = '';
+	//https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80
+	
+</script>
+
+<style>
+
+
+	.cards{
+		display: flex;
+	}
+</style>
+
+
+<input type='text' placeholder="Name" bind:value='{name}' />
+<input type='text' placeholder="Job Title" bind:value='{jobTitle}' />
+<input type='text' placeholder="Description" bind:value='{description}' />
+<input type='text' placeholder="Image URL" bind:value='{imgUrl}' />
+<div class='cards'>
+	<ContactCard userName="{name}" jobTitle="{jobTitle}" description="{description}" imgUrl='{imgUrl}' />
+</div>
+
---
+++ src/ContactCard.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let userName;
+    export let jobTitle;
+    export let description;
+    export let imgUrl;
+
+</script>
+
+<style>
+    .card{
+        border: 1px solid #eee;
+        padding: 1em;
+        margin: 1em;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+        border-radius: 6px;
+    }
+    .thumb{
+    }
+
+    .thumb img {
+        width: 200px;
+        border-radius: 3px;
+    }
+    
+</style>
+
+<div class='card'>
+    <header>
+        <div class="thumb">
+            <img src="{imgUrl}" alt="">
+        </div>
+      <div class='user-data'>
+            <h1>{userName}</h1>
+            <h2>{jobTitle}</h2>
+        </div>
+    </header>
+    <div>
+        <p>{description}</p>
+    </div>
+</div>
